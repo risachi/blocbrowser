00478a2a96f1c5d1bff89ae311a253755c0c1fef
risachi <risachi@gmail.com>
2016-03-22 22:43:19 -0700

    checkpoint-25 success

---
+++ blocBrowser/blocBrowser/AwesomeFloatingToolbar.h
@@ -0,0 +1,31 @@
+//
+//  AwesomeFloatingToolbar.h
+//  blocBrowser
+//
+//  Created by Lisa on 3/21/16.
+//  Copyright © 2016 Lisa Hackenberger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class AwesomeFloatingToolbar;
+
+//this protocol inherits from the NSObject protocol
+@protocol AwesomeFloatingToolbarDelegate <NSObject>
+
+@optional
+
+- (void) floatingToolbar:(AwesomeFloatingToolbar *)toolbar didSelectButtonWithTitle:(NSString *)title;
+
+@end
+
+
+@interface AwesomeFloatingToolbar : UIView
+
+- (instancetype) initWithFourTitles:(NSArray *)titles;
+
+- (void) setEnabled:(BOOL)enabled forButtonWithTitle:(NSString *)title;
+
+@property (nonatomic, weak) id <AwesomeFloatingToolbarDelegate> delegate;
+
+@end
